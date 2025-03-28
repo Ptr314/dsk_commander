dfe5b3b88965be34e6b6c2e5851a9bb2c3b355cd
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-03-28 18:04:50 +0300

    qt version bump

--- .build/update_translations.bat
+++ .build/update_translations.bat
@@ -1,5 +1,5 @@
 @echo off
-SET BUILD_DIR=Desktop_Qt_6_8_2_MinGW_64_bit-Debug
+SET BUILD_DIR=Desktop_Qt_6_8_3_MinGW_64_bit-Debug
 if exist "../src/build/%BUILD_DIR%" (
   cd ../src
   cmake.exe --build "./build/%BUILD_DIR%" --target update_translations
