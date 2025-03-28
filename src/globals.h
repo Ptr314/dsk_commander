29c9efe3f3a3e82839a137002777d979a918c863
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-03-28 20:25:32 +0300

    auto detect fix

--- src/globals.h
+++ src/globals.h
@@ -4,7 +4,7 @@
 #define PROJECT_NAME "DISKCommander"
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "1"
-#define VERSION_PATCH "0"
-#define PROJECT_VERSION "0.1.0"
+#define VERSION_PATCH "1"
+#define PROJECT_VERSION "0.1.1"
 
 #endif // GLOBALS_H_IN
