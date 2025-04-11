4ee4f6c28364b6b152cdf535d757885152b105d0
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-04-11 21:08:02 +0300

    new release

--- src/globals.h
+++ src/globals.h
@@ -3,8 +3,8 @@
 
 #define PROJECT_NAME "DISKCommander"
 #define VERSION_MAJOR "1"
-#define VERSION_MINOR "0"
-#define VERSION_PATCH "1"
-#define PROJECT_VERSION "1.0.1"
+#define VERSION_MINOR "1"
+#define VERSION_PATCH "0"
+#define PROJECT_VERSION "1.1.0"
 
 #endif // GLOBALS_H_IN
