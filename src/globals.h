4ca696b1b01683971d18239c177f3758de319454
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-04-21 19:00:07 +0300

    version bump

--- src/globals.h
+++ src/globals.h
@@ -4,7 +4,7 @@
 #define PROJECT_NAME "DISKCommander"
 #define VERSION_MAJOR "1"
 #define VERSION_MINOR "3"
-#define VERSION_PATCH "3"
-#define PROJECT_VERSION "1.3.3"
+#define VERSION_PATCH "4"
+#define PROJECT_VERSION "1.3.4"
 
 #endif // GLOBALS_H_IN
