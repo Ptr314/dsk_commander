9abb412ee62de0823a965fa598cf9646e38269ec
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-12-14 14:30:28 +0300

    split agat viewers

--- src/globals.h
+++ src/globals.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "DISKCommander"
 #define VERSION_MAJOR "2"
 #define VERSION_MINOR "0"
-#define VERSION_PATCH "0"
-#define PROJECT_VERSION "2.0.0"
+#define VERSION_PATCH "1"
+#define PROJECT_VERSION "2.0.1"
