8e118e8886e3c3b92f7f35212deddcae039a43e7
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-12-15 17:55:46 +0300

    history & project fixes

--- src/globals.h
+++ src/globals.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "DISKCommander"
 #define VERSION_MAJOR "2"
 #define VERSION_MINOR "1"
-#define VERSION_PATCH "0"
-#define PROJECT_VERSION "2.1.0"
+#define VERSION_PATCH "1"
+#define PROJECT_VERSION "2.1.1"
