13e2c5dba32bd33ec225de5d1089a4dfd77d734d
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2026-02-12 16:17:35 +0300

    image refactor

--- src/globals.h
+++ src/globals.h
@@ -2,6 +2,6 @@
 
 #define PROJECT_NAME "DISKCommander"
 #define VERSION_MAJOR "2"
-#define VERSION_MINOR "3"
+#define VERSION_MINOR "4"
 #define VERSION_PATCH "0"
-#define PROJECT_VERSION "2.3.0"
+#define PROJECT_VERSION "2.4.0"
