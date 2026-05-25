27bf246204a5060d7423efb23bc466a11d750a9c
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2026-05-25 12:23:26 +0300

    version bump

--- src/globals.h
+++ src/globals.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "DISKCommander"
 #define VERSION_MAJOR "2"
 #define VERSION_MINOR "6"
-#define VERSION_PATCH "0"
-#define PROJECT_VERSION "2.6.0"
+#define VERSION_PATCH "1"
+#define PROJECT_VERSION "2.6.1"
